0c72d002e6a0677ba7031f1bc2602210f10557a1
Jonathan Flower <jfbloom22@gmail.com>
2024-09-27 17:50:30 -0400

    added links to publications

--- src/components/publication-logos.tsx
+++ src/components/publication-logos.tsx
@@ -5,10 +5,10 @@ import { Card } from '@/components/ui/card'
 
 export const PublicationLogosComponent = () => {
   const publications = [
-    { name: 'SIOP', icon: '/images/siop.png', width: 74, height: 64 },
-    { name: 'Forbes', icon: '/images/Forbes_Logo-BlackOnTrns@svg.svg', width: 120, height: 80 },
-    { name: 'SHRM', icon: '/images/shrm-grey.png', width: 92, height: 72 },
-    { name: 'TechCrunch', icon: '/images/techcrunch-seeklogo.svg', width: 64, height: 64 },
+    { name: 'SIOP', icon: '/images/siop.png', width: 74, height: 64, url: 'https://www.siop.org/Research-Publications/TIP/TIP-Back-Issues/2018/July/ArtMID/20677/ArticleID/1366/7-Questions-and-Answers-About-AI-and-I-O' },
+    { name: 'Forbes', icon: '/images/Forbes_Logo-BlackOnTrns@svg.svg', width: 120, height: 80, url: 'https://www.forbes.com/councils/forbeshumanresourcescouncil/people/neilmorelli/' },
+    { name: 'SHRM', icon: '/images/shrm-grey.png', width: 92, height: 72, url: 'https://www.shrm.org/search-results#q=neil%20morelli&sortCriteria=relevancy%2C%40ytlikecount%20descending' },
+    { name: 'TechCrunch', icon: '/images/techcrunch-seeklogo.svg', width: 64, height: 64, url: 'https://techcrunch.com/author/neil-morelli/' },
   ]
 
   return (
@@ -19,7 +19,10 @@ export const PublicationLogosComponent = () => {
           <div className="grid grid-cols-2 md:grid-cols-4 gap-8 p-6">
             {publications.map((pub) => (
               <div key={pub.name} className="flex flex-col items-center justify-center">
-                <div
+                <a
+                  href={pub.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="relative mb-2 grayscale hover:grayscale-0 transition-all duration-300"
                   style={{ width: `${pub.width}px`, height: `${pub.height}px` }}
                 >
@@ -29,7 +32,7 @@ export const PublicationLogosComponent = () => {
                     layout="fill"
                     objectFit="contain"
                   />
-                </div>
+                </a>
                 {/* <span className="text-sm font-medium text-muted-foreground">{pub.name}</span> */}
               </div>
             ))}
