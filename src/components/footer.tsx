4e271961f4e8d972547778092afd1006be063069
Jonathan Flower <jfbloom22@gmail.com>
2024-10-21 16:25:42 -0400

    update primary color and link to workplace labs

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,6 +5,7 @@ export const Footer = () => {
     <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
       <p className="text-xs text-muted-foreground">&copy; 2024 <Link href="https://www.workplacelabs.io/" className="hover:underline" prefetch={false}>Workplace Labs</Link>. All rights reserved.</p>
       <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+        <Link href="https://www.workplacelabs.io/" className="text-xs hover:underline underline-offset-4" prefetch={false}>Created by Workplace Labs</Link>
         {/* <Link href="#" className="text-xs hover:underline underline-offset-4" prefetch={false}>
           Terms of Service
         </Link>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,9 +3,15 @@ import Link from 'next/link';
 
 export const Header = () => {
   return (
-    <header className="px-4 lg:px-6 h-24 flex items-center border-b border-solid border-border">
-      <Link href="/" className="flex items-center justify-center" prefetch={false}>
-        <Image src="/images/logo-wide.webp" alt="AI Mastermind" width={80} height={80} className="max-h-20 w-auto border border-gray-200 rounded-sm" />
+    <header className="px-4 lg:px-6 h-24 flex items-center border-b border-solid border-border bg-gray-50">
+      <Link href="https://www.workplacelabs.io" className="flex items-center justify-center" prefetch={false}>
+        <Image 
+          src="/images/logo-wide.webp" 
+          alt="AI Mastermind" 
+          width={100} 
+          height={100} 
+          className="max-h-20 w-auto border border-gray-200 rounded-sm transition-transform duration-300 hover:scale-105" 
+        />
         <span className="sr-only">AI Mastermind</span>
       </Link>
       <nav className="ml-auto flex gap-4 sm:gap-6">
